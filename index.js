e5acef5b7fe22420a417214e18614bef5d9270b7
unknown <deepchopda01@gmail.com>
2025-06-06 18:18:32 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const Blog = require("./routes/Blog.route.js")
-const {  mongoose } = require("mongoose")
+const { mongoose } = require("mongoose")
 const app = express()
 const cors = require("cors")
 // const Connect = require("./db/connection.js")
@@ -13,17 +13,17 @@ app.use("/uploads", express.static("uploads"))
 const Auth = require("./routes/Auth.route.js")
 app.use('/auth', Auth)
 app.use("/blog", Blog)
-app.get('/',(req ,res )=>{
+app.get('/', (req, res) => {
     res.send({
-        message : "API running ....."
+        message: "API running ....."
     })
 })
 
 
 
 mongoose.connect(`mongodb+srv://deepchopda01:03YRl4QoQ0o9ipV4@backend.xpjmz.mongodb.net/test_tt`, { useNewUrlParser: true }).then(() => {
-  console.log('database connected');
-  app.listen(5000, async () => {
-    console.log(`server is running on port : 5000`);
-  });
+    console.log('database connected');
+    app.listen(5000, async () => {
+        console.log(`server is running on port : 5000`);
+    });
 });
