b7d147bf718ae8647a39f92e106043854c00c426
chopdadeep <deep3254c@gmail.com>
2025-06-06 19:04:52 +0530

    add cloudinary

--- middleware/multer.js
+++ middleware/multer.js
@@ -16,18 +16,21 @@
 
 
 
+// middleware/upload.js
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../cloudinary.js");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Local folder
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "media", // Cloudinary folder name
+        allowed_formats: ["jpg", "jpeg", "png", "gif", "mp4"],
+        transformation: [{ width: 800, height: 600, crop: "limit" }],
     },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-    }
 });
 
-const upload = multer({ storage: storage });
+const parser = multer({ storage });
 
-module.exports = upload;
+module.exports = parser;
+
