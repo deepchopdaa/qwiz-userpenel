2747be2152d243aefc1ac7455f0f45302bac28fa
unknown <deepchopda01@gmail.com>
2025-06-06 16:42:18 +0530

    6/6

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const Blog = require("./routes/Blog.route.js")
+const {  mongoose } = require("mongoose")
 const app = express()
-const Connect = require("./db/connection.js")
-Connect()
 const cors = require("cors")
+// const Connect = require("./db/connection.js")
+// Connect()
 app.use(cors())
 app.use(express.json())
 
@@ -10,7 +12,6 @@ app.use("/uploads", express.static("uploads"))
 
 const Auth = require("./routes/Auth.route.js")
 app.use('/auth', Auth)
-const Blog = require("./routes/Blog.route.js")
 app.use("/blog", Blog)
 app.get('/',(req ,res )=>{
     res.send({
@@ -20,7 +21,7 @@ app.get('/',(req ,res )=>{
 
 
 
-mongoose.connect(`mongodb+srv://deepchopda01:Deepu1155@backend.xpjmz.mongodb.net/test_tt`, { useNewUrlParser: true }).then(() => {
+mongoose.connect(`mongodb+srv://deepchopda01:03YRl4QoQ0o9ipV4@backend.xpjmz.mongodb.net/test_tt`, { useNewUrlParser: true }).then(() => {
   console.log('database connected');
   app.listen(5000, async () => {
     console.log(`server is running on port : 5000`);
