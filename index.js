d33b12ba6fa518fbaf9597a94867fe6dc8fad093
unknown <deepchopda01@gmail.com>
2025-06-06 16:28:56 +0530

    6/6

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const Auth = require("./routes/Auth.route.js")
 app.use('/auth', Auth)
 const Blog = require("./routes/Blog.route.js")
 app.use("/blog", Blog)
+app.get('/',(req ,res )=>{
+    res.send({
+        message : "API running ....."
+    })
+})
 
 app.listen(5000, () => {
     console.log("App Is Running On 5000")
